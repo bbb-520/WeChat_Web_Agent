da89f7a9291502160eab2e20c10e48750147b9d7
bbb <475260611@qq.com>
2026-07-28 17:53:30 +0800

    feat: add summer-bootstrap module with launcher and config

---
+++ summer-bootstrap/src/main/java/log/summer/bootstrap/SummerApplication.java
@@ -0,0 +1,21 @@
+package log.summer.bootstrap;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+@MapperScan("log.summer.aigc.mapper")
+@ComponentScan(basePackages = {
+        "log.summer.common",
+        "log.summer.aigc",
+        "log.summer.bot"
+})
+public class SummerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SummerApplication.class, args);
+    }
+}
